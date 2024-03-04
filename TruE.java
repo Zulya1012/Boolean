90270b248a392926ff3cc08eb52db4e062a49fe6
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-03-04 16:31:03 +0400

    Add files via upload

---
+++ TruE.java
@@ -0,0 +1,14 @@
+public class TruE{
+  public static void main (String [] arg){
+    int a=10;
+    int b=3;
+    int c=13;
+    
+    boolean amount = checkSum (a, b, c);
+    System.out.println(amount);
+  }
+  public static boolean checkSum (int a, int b, int c){
+    int sum =a+b;
+    return sum==c;
+  }
+}
